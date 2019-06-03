f09ae64f8dda7e5cc1b380723ba5e2e4173ed688
lenanedina <o.nedina@bankvostok.com.ua>
2019-06-03 09:08:19 +0300

    v2

--- lesson 4.1/Program.cs
+++ lesson 4.1/Program.cs
@@ -6,14 +6,52 @@ namespace Lesson4
     {
         static void Main(string[] args)
         {
-            //9.	Одна штука некоторого товара стоит 20,4 грн. Напечатать таблицу стоимости 2, 3, ...,
-            //20 штук …этого товара.
+            //task9();
+            //task265();
+            //task10();
+            task15();
+        }
+         static void task9()
+        //9.	Одна штука некоторого товара стоит 20,4 грн. Напечатать таблицу стоимости 2, 3, ...,
+        //20 штук …этого товара.
+        {
             int x; double y = 20.4;
-            for (x=2; x < 20; x++)
+            for (x = 2; x < 21; x++)
             {
                 Console.WriteLine($"Количество: {x}, Стоимость товара: {y}, Общая стоимость: {x * y}");
             }
-
+        }
+         static void task265()
+        {
+            double a=3, x, b = 0.2, y;
+           
+            for (x = -5; x < 5.5; x += 0.5)
+            {
+                y= 2.05 * a * Math.Pow(x, 3) - 3 * b * Math.Sqrt(5 - Math.Pow(Math.Sin(4 * x), 3)) / Math.Abs(0.3 * Math.Pow(Math.Tanh(x - 2), 3));
+                    
+                               Console.WriteLine($"ЗНАЧЕНИЕ X: {x}, РЕЗУЛЬТАТ Y: {y}");
+             }
+        }
+        static void task10()
+        {
+            double q, x = 0.423; 
+            for (q = 1; q < 11; q++)
+            {
+                Console.WriteLine($"Вес в фунтах {q} - Вес в килограммах {q*x}");
+            }
+        }
+        static void task15()
+        {//таблица умножения на 7
+            for (int e = 7; e < 11;e++)
+            { for (int r = 1; r < 11; r++)
+                {
+                    if (e >7)
+                    break;
+                    Console.WriteLine($"{e} * {r} = {e*r}");
+                }
+            }
+           
         }
     }
 }
+
